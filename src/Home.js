b5b535afb09c8a2badc661f00e3689b440f21e2a
harish <hmahajan.dmi@gmail.com>
2017-11-23 19:14:54 +0530

    harish1234

--- src/Home.js
+++ src/Home.js
@@ -51,7 +51,7 @@ class Home extends Component {
   render() {
     return (
         <div>
-            <h1>Home1</h1>
+            <h1>Home1234</h1>
             Crated By:<b> <i> Mahajan Harish </i></b> <br/>
             Email: <b><i>hmahajan.dmi@gmail.com </i></b> <br/>
             Contact: <b><i>+91 8000641661</i></b>
